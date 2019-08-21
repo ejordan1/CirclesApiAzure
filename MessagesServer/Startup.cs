ba64b68a142af0e0a1a8097e5ca4f650979dd53f
Student <example@example.com>
2019-08-21 13:18:30 -0700

    change startup file

--- MessagesServer/Startup.cs
+++ MessagesServer/Startup.cs
@@ -58,12 +58,22 @@ namespace MessagesServer
             }
             else
             {
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
             //app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseMvc(routes =>
+             {
+                 routes.MapRoute(
+                     name: "default",
+                     template: "{controller=Messages}/{action=Index}/{id?}");
+             });
         }
     }
 }
